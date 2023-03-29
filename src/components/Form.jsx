aa6253c26dafba7a3480deda4454c09aca99c03a
nandhakumaran7 <nandhamadhankumar2004@gmail.com>
2023-03-29 14:51:46 +0530

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import Button from '@material-ui/core/Button';
+const FRUITS = ['Apple', 'Banana', 'Cherry', 'Durian', 'Elderberry'];
+
+function FruitSurvey() {
+  const [name, setName] = useState('');
+  const [fruit, setFruit] = useState('');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Hello, ${name}! Your favorite fruit is ${fruit}.`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="ff">
+      <TextField
+        label="Name"data-testid="name"value={name} onChange={(event) => setName(event.target.value)} className="nn" 
+      />
+      <Autocomplete data-testid="autocomplete" options={FRUITS}getOptionLabel={(option) => option}
+      renderInput={(params) => (
+          <TextField {...params}label="Choose a fruit"variant="outlined" className='tt'/> )}
+          value={fruit}onChange={(event, value) => setFruit(value)}/>
+      <Button data-testid="button"type="submit"variant="outlined"color="primary">Submit</Button>
+    </form>
+  );
+}
+
+export default FruitSurvey;
